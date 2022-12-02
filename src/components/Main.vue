6e099ae1142d4a1d74f3e320e136f3c4ff94706e
DarioRossi99 <rssdra99@gmail.com>
2022-12-02 17:24:34 +0100

    inserimento dell header

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default{
+
+};
+</script>
+
+<template>
+
+    <div class="container">
+        <h2 class="momento"><strong></strong></h2>
+    </div>
+  
+</template>
+
+<style scoped lang="scss">
+@use "../styles/general.scss" as * ;
+  
+</style>
