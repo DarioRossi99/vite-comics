9da27bdbd138de03cb78e97cda0150ec9b157dbb
DarioRossi99 <rssdra99@gmail.com>
2022-12-03 17:45:09 +0100

    inserimento main più sfondo

---
+++ src/components/Header.vue
@@ -0,0 +1,67 @@
+<script>
+
+export default{
+
+};
+</script>
+
+<template>
+  <section>
+
+      <div class="container">
+        <div>
+          <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="link">
+          <ul>
+            <a href="#">CHARACTERS</a>
+            <a href="#">COMICS</a>
+            <a href="#">MOVIES</a>
+            <a href="#">TV</a>
+            <a href="#">GAMES</a>
+            <a href="#">COLLECTIBLES</a>
+            <a href="#">VIDEOS</a>
+            <a href="#">FANS</a>
+            <a href="#">NEWS</a>
+            <a href="#">SHOP</a>
+          </ul>
+        </div>
+      </div>
+
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/general.scss" as * ;
+
+  a{
+    text-decoration: none;
+    color: black;
+  }
+  .backGround{
+    background-color: $Bg-header;
+  }
+  .container{
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+
+  .link{
+    width: 60%;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  ul{
+    display: contents;
+  }
+  .listaLink{
+    display: flex;
+    justify-content: space-between;
+    width: 65%;
+  }
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,13 +7,92 @@ export default{
 
 <template>
 
-    <div class="container">
-        <h2 class="momento"><strong></strong></h2>
-    </div>
-  
+    <section id="bg-sezione">
+        <div class="container">
+            <div class="fumetti">
+                <h2>Content goes here</h2>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div class="shop">
+            <div class="card">
+                <img class="immagini" src="../assets/img/buy-comics-digital-comics.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+
+            <div class="card">
+                <img class="immagini" src="../assets/img/buy-comics-merchandise.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+            
+            <div class="card">
+                <img class="immagini" src="../assets/img/buy-comics-subscriptions.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+
+            <div class="card">
+                <img class="immagini" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+
+            <div class="card">
+                <img class="immagini" src="../assets/img/buy-dc-power-visa.svg" alt="">
+                <a href="#">DIGITAL COMICS</a>
+            </div>
+        </div>      
+    </section>
+
+    <section id="img-sfondo-sezione">
+
+    </section>
 </template>
 
 <style scoped lang="scss">
 @use "../styles/general.scss" as * ;
+
+#bg-sezione{
+    background-color:$Bg-main-primo;
+}
+
+#img-sfondo-sezione{
+    background-image: url(../assets/img/footer-bg.jpg);
+    height: 30rem;
+}
+.fumetti{
+    padding: 3rem 0;
+    background-color: $Bg-main-primo;
+    color: $Bg-header;
+}
+
+.shop{
+    padding: 3rem;
+    background-color: $Bg-main-secondo;
+    color: $Bg-header;
+    display: flex;
+    justify-content: center;
+}
+
+.card{
+    border: none;
+    background-color: $Bg-main-secondo;
+    display: block;
+}
+
+.immagini{
+    background-color: $Bg-main-secondo;
+    width: 40px;
+    object-fit: cover;
+    height: -webkit-fill-available;
+}
+
+a{
+    text-decoration: none;
+    color: white;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+
   
 </style>
